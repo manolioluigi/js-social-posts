a132550ad09eb4dfed26b40d97f6952ad44ab773
manolioluigi <luigi.manolio1999@gmail.com>
2022-12-15 18:01:39 +0100

    fix href e bonus 3

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ posts.forEach((element) => {
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                        <a class="like-button  js-like-button" data-postid="${element.id}">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
